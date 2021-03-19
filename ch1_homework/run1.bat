7cea96c0641ae7f01648a334353bdff6923e4c06
zeng <confidentoh@foxmail.com>
2021-03-19 23:56:49 +0800

    finished ch1

---
+++ ch1_homework/run1.bat
@@ -0,0 +1,7 @@
+set JAVA_HOME=D:\java11
+set PROJECT_HOME=D:\JavaHomework\ch1_homework
+set path=%path%;%JAVA_HOME%\bin
+set classpath=%classpath%;%PROJECT_HOME%\chapter1\class
+
+
+java -classpath %classpath% homework.ch1.Welcome
